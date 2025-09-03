9dcc5ce406b47316f0da33e852321b10c71edc7d
Luis Almaraz <luis-almaraz03@outlook.com>
2025-09-03 17:35:25 -0600

    Delet/new(Dir_Ejercicios arreglos/Ejercicios_Arreglos.h) Union de los trabajos dentro de la carpeta ejercicos arreglos al archivo nuevo

--- Ejercicios_Arreglos.h
+++ Ejercicios_Arreglos.h
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void Arreglos_1() {
+    int arr[8];
+    cout << "Ingrese 8 números enteros:" << endl;
+    for (int i = 0; i < 8; ++i) {
+        cin >> arr[i];
+    }
+
+    for (int i = 1; i < 8; ++i) {
+        if(arr[i]<arr[i-1]){
+            int temp = arr[i];
+            arr[i] = arr[i-1];
+            arr[i-1] = temp;
+            i=0;
+        }
+    }
+
+    cout << "Arreglo ordenado: ";
+    for (int i = 0; i < 8; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void Arreglos_2() {
     const int TAM = 14;
     cout << "Ingrese una palabra de 14 caracteres: ";
     string pal;
@@ -21,5 +44,4 @@ int main() {
     if (!encontrado) {
         cout << "DATO NO LOCALIZADO" << endl;
     }
-    return 0;
 }
