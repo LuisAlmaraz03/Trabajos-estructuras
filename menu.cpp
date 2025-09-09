d5c227d3d6b6819a71a1e00a27dfbb9ef449dd05
EmilianoR <emigsmer17@gmail.com>
2025-09-08 20:45:21 -0600

    (menu.cpp) se agrego la opcion de algoritmos bidimensionales

--- menu.cpp
+++ menu.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include "Ejercicios_Recursividad.h"
 #include "Ejercicios_Arreglos.h"
+#include "ArreglosBi.h"
 using namespace std;
 
 int main() {
@@ -8,6 +9,7 @@ int main() {
     cout<<"Menu de compilados\n";
     cout<<"1. Algortimos de Recursividad\n";
     cout<<"2. Algoritmos de Arreglos Unidimensionales"<<endl;
+    cout<<"3. Algoritmos de Arreglos Bidimensionales"<<endl;
     cin>>opc;
     switch(opc) {
         int opc2;
@@ -74,7 +76,49 @@ int main() {
             break;
         }
         break;
-    default:
+       case 3:
+       cout<<"Has seleccionado Algoritmos de Arreglos Bidimensionales\n";
+       cout<<"1. Palindromo de una matriz\n";
+       cout<<"2. Suma de matrices\n";
+       cout<<"3. Columnas a una potencia\n";
+       cout<<"4. Suma de Renglones\n";
+       cout<<"5.Multiplicacion de matrices\n";
+       cout<<"6.Determinante de una matriz 3x3\n";
+       cout<<"7. Conteo de ceros, positivos y negativos en una matriz 5x4\n";
+       cout<<"8.Mayor y menor en una matriz 4x4\n";
+       cin>>opc2;
+         switch (opc2){
+        case 1:
+            ArreglosBi_1();
+            break;
+        case 2:
+            ArreglosBi_2();
+            break;
+        case 3:
+            ArreglosBi_3();
+            break;
+        case 4:
+            ArreglosBi_4();
+            break;
+        case 5:
+            ArreglosBi_5(); 
+            break;
+        case 6:
+            ArreglosBi_6();
+            break;
+        case 7:
+            ArreglosBi_7();
+            break;
+        case 8:
+            ArreglosBi_8(); 
+            break;
+        default:
+            cout<<"Opcion no valida"<<endl;
+            break;
+         }
+    
+
+        default:
         cout<<"Opcion no valida"<<endl;
         break;
     }
