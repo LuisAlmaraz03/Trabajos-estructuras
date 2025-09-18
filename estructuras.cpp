a02deecf05dd70a7adef2e69683b6e51b6d5646f
EmilianoR <emigsmer17@gmail.com>
2025-09-17 20:17:31 -0600

    (estructuras.cpp) , problea 3 ya bien

--- estructuras.cpp
+++ estructuras.cpp
@@ -1,51 +1,77 @@
 #include<iostream>
 using namespace std;
-#define cantidad 2
-
+#define max 10
 int main(){
-    struct datos
-    {
-        string cuenta;
+    struct datos{
+        string nombre;
         int edad;
         char sexo;
-        float calificacion1;
-        float calificacion2;
-        float promedio;
+        struct domicilio{
+            string calle;
+            int numero;
+            string ciudad;
+        } direccion;
+        int telefono;
+        bool asegurado;
+
     };
-    datos alumnos[cantidad];
-    float prom_gen;
-    int buscador;
-    int count= 1;
+    datos persona[max];
+    int n;
+    string nombrebuscado;
+    cout<<"Cuantas personas desea registrar? (max 10)"<<endl;
+    cin>>n;
+    cin.ignore();
+    int counta=0,countb=0,sumamenores=0,sumamujeres=0,countm=0;
+    for(int i=0;i<n;i++){
+        cout<<"Nombre:  "<<endl;
+       getline(cin, persona[i].nombre);
+        cout<<"Edad:  "<<endl;
+        cin>>persona[i].edad;
+        cin.ignore();
+        if(persona[i].edad<13){
+            countb++;
+        }
+        cout<<"Sexo:  "<<endl;
+        cin>>persona[i].sexo;
+        cin.ignore();
+         if(persona[i].sexo=='F'||persona[i].sexo=='f'){
+            countm++;
+        }
+        cout<<"Calle:  "<<endl;
+        getline(cin, persona[i].direccion.calle);
 
-    for(int i=0; i<cantidad; i++){
-    cout<<"Cuenta\n";
-    cin>>alumnos[i].cuenta;
-    cout<<"Edad\n";
-    cin>>alumnos[i].edad;
-    cout<<"Sexo\n";
-    cin>>alumnos[i].sexo;
-    cout<<"Calificacion 1\n";
-    cin>>alumnos[i].calificacion1;
-    cout<<"Calificacion 2\n";
-    cin>>alumnos[i].calificacion2;
-    alumnos[i].promedio=alumnos[i].calificacion1 + alumnos[i].calificacion2; 
-   }
-
-    cout<<"Edad a buscar\n";
-    cin>>buscador;
-   for (int i = 0; i < cantidad; i++)
-   {
-    if (alumnos[i].sexo == 'F'){
-        cout<<"\nAlumna "<<count<<alumnos[i].cuenta;
-        count++;
+        cout<<"Numero:  "<<endl;
+        cin>>persona[i].direccion.numero;
+        cin.ignore();
+        cout<<"Ciudad:  "<<endl;
+        getline(cin, persona[i].direccion.ciudad);
+        cout<<"Telefono:  "<<endl;
+        cin>>persona[i].telefono;
+        cin.ignore();
+        cout<<"Asegurado (1=si/0=no):  "<<endl;
+        cin>>persona[i].asegurado;
+        cin.ignore();
+        if(persona[i].asegurado==1){
+            counta++;
+        }
     }
-    else if ()
-    {
-        /* code */
+    cout<<"Lista de los nombres de los pacientes hospitalizados: "<<endl;
+    for(int i=0;i<n;i++){
+        cout<<persona[i].nombre<<endl;
+    }
+    cout<<"Porcentaje de edad de los menores de 13 años: "<<(countb/n)*100<<"%"<<endl;
+    cout<<"Numero de personas aseguradas: "<<counta<<endl;
+    cout<<"Porcentaje de mujeres hospitalizadas: "<<(countm/n)*100<<"%"<<endl;
+    cout<<"Ingresa un nombre de un paciente para darte los datos de su domicilio: "<<endl;
+    getline(cin,nombrebuscado);
+    for(int i=0;i<n;i++){
+        if(persona[i].nombre==nombrebuscado){
+            cout<<"Calle: "<<persona[i].direccion.calle<<endl;
+            cout<<"Numero: "<<persona[i].direccion.numero<<endl;
+            cout<<"Ciudad: "<<persona[i].direccion.ciudad<<endl;
+        }
     }
 
-    cout<<"\nJugador "<<i+1<<"\nNombre:"<<jugadores[i].nombre<<"\nEdad: "<<jugadores[i].edad<<"\nAltura: "<<
-    jugadores[i].altura<<"\nSexo: "<<jugadores[i].sexo<<endl;
-   }
+
 
 }
