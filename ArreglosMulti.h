8a6d3e9fcbbefb40857966e49b7eb5639deb49fa
Luis Almaraz <luis-almaraz03@outlook.com>
2025-09-24 17:48:29 -0600

    New function/Delet(ArreglosMlti.h/conjuntos.cpp) Se agrego el trabajo de los estudiantes, se elimino conjuntos ya no es necesario

--- ArreglosMulti.h
+++ ArreglosMulti.h
@@ -23,10 +23,53 @@ for(int k=0; k<anio; k++){
 
             
         }
-      }
-      cout<<"El promedio anual es: "<<suma/(anio*mes*dia)<<endl;
-                suma=0;
+    }
+    cout<<"El promedio anual es: "<<suma/(anio*mes*dia)<<endl;
+        suma=0;
     }
     cout<<"El promedio del mes "<<mesEsp<<" es: "<<suma2/(anio*dia)<<endl;
+}
+
+void Arreglosmulti_2(){
+    int examenes, estudiantes, cursos, count=0;
+    char genero;
+    cout<<"Ingrese la cantidad de estudiantes: ";
+    cin>>estudiantes;
+    cout<<"Ingrese la cantidad de cursos: ";
+    cin>>cursos;
+    cout<<"Ingrese la cantidad de examenes: ";
+    cin>>examenes;
     
+    float calificaciones[estudiantes][cursos][examenes+1]; // Promedio final por curso
+    for(int i=0;i<estudiantes;i++){
+        cout<<"Estudiante "<<i+1<<". ";
+        for(int j=0;j<cursos;j++){
+            cout<<"Curso "<<j+1<<".\n";
+            calificaciones[i][j][examenes]=0; 
+            for(int k=0;k<examenes;k++){
+                cout<<"Examen "<<k+1<<": ";
+                cin>>calificaciones[i][j][k];
+                calificaciones[i][j][examenes]+=calificaciones[i][j][k];
+            }
+            if (j==cursos-1){
+                cout<<"Cual es el genero del estudiante (M/F): ";
+                cin>>genero;
+                if(genero=='F' || genero=='f'){
+                    count++;
+                }
+            }
+        }
+        cout<<endl;
+    }
+    cout<<"Cantidad de estudiantes mujeres en el grupo: "<<count<<endl;
+    cout<<"Promedios Final: "<<endl;
+    for(int i=0;i<estudiantes;i++){
+        float suma=0;
+        cout<< "Estudiante "<<i+1<<". ";
+        for(int j=0;j<cursos;j++){
+            suma+=calificaciones[i][j][examenes];
+        }
+        cout<<"Promedio Final: "<<suma/(cursos*examenes)<<endl;
+        suma=0;
+    }
 }
