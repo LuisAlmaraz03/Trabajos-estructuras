508f68359b7f1bc0511ba9e794884175caad86a8
Fabian <119006243@pcpuma.acatlan.unam.mx>
2025-10-13 12:50:03 -0600

    New(Compilacion.cpp) Se crearon los 3 primeros ejercicios del 13/10

--- Compilacion.cpp
+++ Compilacion.cpp
@@ -10,6 +10,10 @@ bool crarListaFinal(NodoDoble* &P, NodoDoble* &Q, bool &verificador);
 void imprimirLista(NodoDoble* P);
 bool eliminarUltimo(NodoDoble* &P ,NodoDoble* &Q, bool &verificador);
 bool eliminarCoincidencia(NodoDoble* &P,NodoDoble* &Q,bool &veficador);
+void eliminarAntesDeOtro(NodoDoble* &P, NodoDoble* &Q);
+void insertarAlInicio(NodoDoble* &P);
+void insertarDespuesDeOtro(NodoDoble* &P, NodoDoble* &Q);
+void palindromo ();
 
 int main(){
     typedef NodoDoble* PNodo;
@@ -19,7 +23,9 @@ int main(){
     char resp='s';
     crarListaFinal(P,Q,verificador);
     imprimirLista(P);
-    eliminarCoincidencia(P,Q,verificador);
+    eliminarAntesDeOtro(P,Q);
+    imprimirLista(P);
+    insertarDespuesDeOtro(P,Q);
     imprimirLista(P);
 }
 bool crarListaFinal(NodoDoble* &P, NodoDoble* &Q, bool &verificador){
@@ -111,4 +117,79 @@ bool eliminarCoincidencia(NodoDoble* &P,NodoDoble* &Q,bool &veficador){
         delete aux;
     }
     return veficador=true;
+}
+
+void eliminarAntesDeOtro(NodoDoble* &P, NodoDoble* &Q){
+    typedef NodoDoble* PNodo;
+    int eliminado;
+    PNodo aux;
+    aux=P;
+    int Buscar;
+    cout<<"Antes de que dato eliminaras?\n";
+    cin>>Buscar;
+    while (aux!=nullptr && aux->dato!=Buscar){
+        aux=aux->ligaD;
+    }
+    if (aux==nullptr){
+        cout<<Buscar<<"No existe en la lista"<<endl;
+    }
+    else if(aux==P){
+        cout<<"No existe un dato anterior a "<<Buscar<<endl;
+    }
+    else if(aux->ligaI==P){
+        delete P;
+        P=aux;
+        aux->ligaI = nullptr;
+    }
+    else{
+        aux=aux->ligaI;
+        aux->ligaI->ligaD=aux->ligaD;
+        aux->ligaD->ligaI=aux->ligaI;
+        delete aux;
+    }
+}
+
+void insertarAlInicio(NodoDoble* &P){
+    typedef NodoDoble* PNodo;
+    PNodo aux;
+    aux=P;
+    P= new NodoDoble;
+    cout<<"Ingresa el nuevo dato\n";
+    cin>>P->dato;
+    P->ligaI=nullptr;
+    P->ligaD=aux;
+    aux->ligaI=P;
+}
+
+void insertarDespuesDeOtro(NodoDoble* &P, NodoDoble* &Q){
+    typedef NodoDoble* PNodo;
+    int Buscar;
+    PNodo aux,T;
+    cout<<"Despues de que dato ingresaras el nuevo?\n";
+    cin>>Buscar;
+    aux=P;
+    while (aux!=nullptr && aux->dato!=Buscar){
+        aux=aux->ligaD;
+    }
+    if(aux==nullptr){
+        cout<<Buscar<<" No existe en la lista"<<endl;
+    }
+    else if(aux==Q){
+        aux=new NodoDoble;
+        cout<<"Ingresa el nuevo dato\n";
+        cin>>aux->dato;
+        aux->ligaD=nullptr;
+        aux->ligaI=Q;
+        Q->ligaD=aux;
+        Q=aux;
+    }
+    else{
+        T=new NodoDoble;
+        cout<<"Ingresa el nuevo dato\n";
+        cin>>T->dato;
+        T->ligaD=aux->ligaD;
+        aux->ligaD->ligaI=T;
+        aux->ligaD=T;
+        T->ligaI=aux;
+    }
 }
